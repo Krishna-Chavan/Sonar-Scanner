b5f8655791e355c40a0238eb1a66ca78b1038ee2
krishna.chavan <krishna.chavan@brillio.com>
2022-01-19 08:51:55 +0530

    sonar-scanner

---
+++ src/main/java/com/brillio/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.brillio;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World code for sonarqube scanner.");
+		//C:\Users\krishna.chavan\Desktop\AWS\SonaeQubeScanner\src\main
+	}
+}
